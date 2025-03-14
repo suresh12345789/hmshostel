f6bcacc7bf2d599978d350e3f681cf4956e59a5b
TullibilliNaresh <nareshtullibilli909@gmail.com>
2025-03-14 18:27:18 +0530

    changes has been done

--- src/Component/Home/Home.jsx
+++ src/Component/Home/Home.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import mobile from "../../Asstes/images/Home/mobileseeing.jpg";
 import banner from '../../Asstes/images/Home/hostelbanner.png'
 
 function Home() {
@@ -7,38 +6,6 @@ function Home() {
 
 
     <div>
-    {/* <div>
-      <div className="grid grid-cols-2 mt-3 mx-10 gap-5 ">
-        <div className="bg-[#efd4d4] rounded-lg p-10">
-          <h1 className="text-4xl  font-semibold leading-tight">
-            Efficiently manage bookings, rooms, payments, users, feedback, and
-            services.
-          </h1>
-          <h2 className="text-2xl pt-10">
-            Manage bookings, rooms, payments, users, feedback, services, and
-            reports.
-          </h2>
-          <button className=" bg-gradient-to-r from-[#787BFF] via-[#D67DFF] to-[#F39A55] px-10 py-3 mt-12 rounded-full">
-            <h1 className="text-lg text-white font-semibold">
-              Start with a free palne
-            </h1>
-          </button>
-          <h1 className="text-md pt-10">
-            Experience our Start with a free palne!
-          </h1>
-        </div>
-
-        <div>
-          <img
-            src={mobile}
-            alt=""
-            className="w-full h-[500px] object-cover rounded-lg "
-          />
-        </div>
-      </div>
-    </div> */}
-
-
     <div>
     <div className=" mt-5 mx-10 relative">
       <img src={banner} alt="" className="w-full    rounded-s-lg  object-cover" />
--- src/Component/Home/Mainfeatures.jsx
+++ src/Component/Home/Mainfeatures.jsx
@@ -1,91 +1,47 @@
 import React, { useState } from "react";
 import mobile from "../../Asstes/images/Home/houselpng.png";
-import { FaArrowAltCircleRight } from "react-icons/fa";
 import { FaArrowRightLong } from "react-icons/fa6";
-import { FaAngleUp, FaAngleDown } from "react-icons/fa6";
-import { GoArrowRight } from 'react-icons/go';
 import { GoArrowUp } from "react-icons/go";
 import { BsArrowDown } from "react-icons/bs";
 
 function Mainfeatures() {
-  // const accordionItems = [
-  //   {
-  //     id: 1,
-  //     title: "According to the manager",
-  //     text: "A paragraph is a group of sentences that develop a single idea or topic. Paragraphs help organize writing, making it easier for readers to follow the author's ideas.",
-  //   },
-  //   {
-  //     id: 2,
-  //     title: "According to the manager",
-  //     text: "A paragraph is a group of sentences that develop a single idea or topic. Paragraphs help organize writing, making it easier for readers to follow the author's ideas.",
-  //   },
-  //   {
-  //     id: 3,
-  //     title: "According to the manager",
-  //     text: "A paragraph is a group of sentences that develop a single idea or topic. Paragraphs help organize writing, making it easier for readers to follow the author's ideas.",
-  //   },
-  // ];
+  const data = [
+    {
+      id: 1,
+      title: "Tenants",
+      text: "The near-infrared light spectrum is not visible to the human eye, and therefore any light that has a wavelength greater than around 750nm, you will not be able to see with your naked eye. Your body will still be feeling all the powers of the light wavelengths though!",
+      icon1: <GoArrowUp />,
+      icon2: <BsArrowDown />
+    },
+    {
+      id: 2,
+      title: "Contracts and bills",
+      text: "The near-infrared light spectrum is not visible to the human eye, and therefore any light that has a wavelength greater than around 750nm, you will not be able to see with your naked eye. Your body will still be feeling all the powers of the light wavelengths though!",
+      icon1: <GoArrowUp />,
+      icon2: <BsArrowDown />
+    },
+    {
+      id: 3,
+      title: "Utilities and rates",
+      text: "Both Red Light and NIR light are a part of the light spectrum of the sun. Red Light emits wavelengths of between 600-700 nm and near-Infrared Light (NIR) is invisible to the naked eye and has a wavelength of 700nm to 1000nm. NIR can penetrate the skin much deeper than red light, including all soft tissue, connective tissue, and bone.",
+      icon1: <GoArrowUp />,
+      icon2: <BsArrowDown />
+    },
+    {
+      id: 4,
+      title: "Fixtures and inventory",
+      text: "The near-infrared light spectrum is not visible to the human eye, and therefore any light that has a wavelength greater than around 750nm, you will not be able to see with your naked eye. Your body will still be feeling all the powers of the light wavelengths though!",
+      icon1: <GoArrowUp />,
+      icon2: <BsArrowDown />
+    }
+  ];
 
-  // const [activeIndex, setActiveIndex] = useState(null);
+  const [openIndex, setOpenIndex] = useState(null);
 
-  // const handleToggle = (index) => {
-  //   setActiveIndex(activeIndex === index ? null : index);
-  // };
-
-
-    const data = [
-        {
-            id: 1,
-            title: "Tenants",
-            text: "The near-infrared light spectrum is not visible to the human eye, and therefore any light that has a wavelength greater than around 750nm, you will not be able to see with your naked eye. Your body will still be feeling all the powers of the light wave lengths though!",
-            icon1: <GoArrowUp />,
-            icon2: <BsArrowDown />
-        },
-
-        {
-          id: 1,
-          title: "Contracts and bills",
-          text: "The near-infrared light spectrum is not visible to the human eye, and therefore any light that has a wavelength greater than around 750nm, you will not be able to see with your naked eye. Your body will still be feeling all the powers of the light wave lengths though!",
-          icon1: <GoArrowUp />,
-          icon2: <BsArrowDown />
-      },
-        
-        {
-            id: 1,
-            title: "Utilities and rates",
-            text: "Both Red Light and NIR light are a part of the light spectrum of the sun. Red Light emits wavelengths of between 600-700 nm and near-Infrared Light (NIR) is invisible to the naked eye and has a wavelength of 700nm to 1000nm. NIR can penetrate the skin much deeper than red light, including all soft tissue, connective tissue, and bone. ",
-            icon1: <GoArrowUp />,
-            icon2: <BsArrowDown />
-        },
-        {
-            id: 1,
-            title: "Fixtures and inventary",
-            text: "The near-infrared light spectrum is not visible to the human eye, and therefore any light that has a wavelength greater than around 750nm, you will not be able to see with your naked eye. Your body will still be feeling all the powers of the light wave lengths though!",
-            icon1: <GoArrowUp />,
-            icon2: <BsArrowDown />
-        }
-    ];
-
-    const [openIndex, setOpenIndex] = useState(null);
-    const [openTimes, setOpenTimes] = useState([]);
-
-    const toggleAccordion = (index) => {
-        const currentTime = new Date().getTime();
-
-        if (openIndex === index) {
-
-            const duration = currentTime - openTimes[index].openTime;
-            console.log(`Accordion ${index + 1} was open for ${duration} ms`);
-            setOpenIndex(null);
-        } else {
-            setOpenIndex(index);
-            setOpenTimes((prev) => {
-                const newTimes = [...prev];
-                newTimes[index] = { openTime: currentTime };
-                return newTimes;
-            });
-        }
-    };
+  const toggleAccordion = (index) => {
+    // Toggle the accordion by setting the openIndex to the current index
+    setOpenIndex(openIndex === index ? null : index);
+  };
 
   return (
     <div>
@@ -94,7 +50,7 @@ function Mainfeatures() {
           <div className="flex gap-10 items-center">
             <div className=" mt-7 ml-32">
               <button className=" text-white font-semibold text-md bg-gradient-to-r bg-[#19467D] py-4 px-16 rounded-full">
-                Start whith a free plane
+                Start with a free plan
               </button>
             </div>
             <div>
@@ -105,14 +61,11 @@ function Mainfeatures() {
           </div>
         </div>
 
-
         <div className="flex gap-5 mt-10 ">
-              <div className="bg-[#F2FDA7] mt-1 w-6 h-6 rounded-full shadow-md "></div>
-              <h1 className="text-3xl  md:text-[40px] font-semibold">
-                {" "}
-                Main features
-              </h1>
-            </div>
+          <div className="bg-[#F2FDA7] mt-1 w-6 h-6 rounded-full shadow-md "></div>
+          <h1 className="text-3xl  md:text-[40px] font-semibold">Main features</h1>
+        </div>
+        
         <div className="grid-cols-2 grid mt-5 gap-5">
           <div className="relative">
             <img
@@ -120,9 +73,6 @@ function Mainfeatures() {
               alt=""
               className="rounded-2xl h-[550px] w-full object-cover"
             />
-       
-
-         
           </div>
 
           <div>
@@ -141,7 +91,7 @@ function Mainfeatures() {
                   <h2 className="text-lg">
                     <FaArrowRightLong />
                   </h2>
-                  <h1 className="text-lg">Maplocation</h1>
+                  <h1 className="text-lg">Map location</h1>
                 </div>
               </div>
 
@@ -163,77 +113,34 @@ function Mainfeatures() {
             </div>
 
             {/* Accordion */}
+            <div className="rounded-lg space-y-5 mt-10">
+              {data.map((service, index) => (
+                <div key={service.id} className="rounded-xl bg-[#f2ece2] overflow-hidden mt-5">
+                  <button
+                    onClick={() => toggleAccordion(index)}
+                    className="w-full space-x-6 rounded-t-xl flex justify-between p-5 bg-[#f2ece2] text-slate-800 focus:outline-none"
+                  >
+                    <span className="text-start text-xl font-semibold pr-10">{service.title}</span>
+                    <span className="text-black transition-transform duration-300">
+                      {openIndex === index ? service.icon1 : service.icon2}
+                    </span>
+                  </button>
 
-<<<<<<< HEAD
-            <div className="w-full h-auto bg-orange-400 flex justify-center mt-10">
-=======
-            {/* <div className="w-full h-auto bg-orange-400 flex justify-center mt-10">
->>>>>>> e11df2fac78cb62938117a2c037890726beac7f7
-              <div className="w-full border rounded-lg overflow-hidden bg-white text-black">
-                <div className="space-y-10">
-                  {" "}
-            
-                  {accordionItems.map((item, index) => (
-                    <div key={item.id}>
-                      <div
-                        className={`px-5 py-5 flex items-center justify-between cursor-pointer ${
-                          activeIndex === index
-                            ? "bg-green-500"
-                            : "bg-orange-400"
-                        }`}
-                        onClick={() => handleToggle(index)}
-                      >
-                        <p className="flex-1">{item.title}</p>
-                        <span>
-                          {activeIndex === index ? (
-                            <FaAngleUp />
-                          ) : (
-                            <FaAngleDown />
-                          )}
-                        </span>
-                      </div>
-                      {activeIndex === index && (
-                        <div className="px-5 py-5">
-                          <p>{item.text}</p>
-                        </div>
-                      )}
+                  {openIndex === index && (
+                    <div
+                      className="text-gray-400 bg-[#f2ece2] text-sm px-5 pb-3 rounded-b-xl"
+                      style={{
+                        maxHeight: openIndex === index ? '200px' : '0',
+                        overflow: 'hidden',
+                        transition: 'max-height 0.3s ease-in-out',
+                      }}
+                    >
+                      {service.text}
                     </div>
-                  ))}
+                  )}
                 </div>
-              </div>
-            </div> */}
-
-
-<div className="rounded-lg space-y-5">
-                    {data.map((service, index) => (
-                 
-                        <div key={service.id} className="rounded-xl bg-[#f2ece2] overflow-hidden mt-5">
-                            <button
-                                onClick={() => toggleAccordion(index)}
-                                className="w-full space-x-6 rounded-t-xl flex justify-between p-5 bg-[#f2ece2] text-slate-800 focus:outline-none"
-                            >
-                                <span className="text-start text-xl font-semibold pr-10">{service.title}</span>
-                                <span className="text-black transition-transform duration-300">
-                                    {openIndex === index ? service.icon1 : service.icon2}
-                                </span>
-                            </button>
-
-                            {openIndex === index && (
-                                <div
-                                    className="text-gray-400 bg-[#f2ece2] text-sm px-5 pb-3 rounded-b-xl"
-                                    style={{
-                                        maxHeight: openIndex === index ? '200px' : '0',
-                                        overflow: 'hidden',
-                                        transition: 'max-height 0.3s ease-in-out',
-                                    }}
-                                >
-                                    {service.text}
-                                </div>
-                            )}
-                        </div>
-
-                    ))}
-                </div>
+              ))}
+            </div>
           </div>
         </div>
       </div>

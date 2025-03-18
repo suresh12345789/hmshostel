da643ed58c8d6d0099dc2125aea28121c1fac87c
suresh12122121 <chittirisuresh@gmail.com>
2025-03-18 17:22:37 +0530

    changes home and benifit and

--- src/Component/Home/Mainfeatures.jsx
+++ src/Component/Home/Mainfeatures.jsx
@@ -1,37 +1,44 @@
 import React, { useState } from "react";
 
-import mobile from "../../Asstes/images/Home/mobilehostel.png"
+import mobile from "../../Asstes/images/Home/mobile1.png"
 import { GoArrowUp } from "react-icons/go";
 import { BsArrowDown } from "react-icons/bs";
-import { FaArrowRightLong } from "react-icons/fa6";
+
 
 function Mainfeatures() {
   const data = [
     {
       id: 1,
-      title: "Tenants",
-      text: "The near-inflight spectrum is not visible to the human eye, and therefore any light that has a wavelength greater than around 750nm, you will not be able to see with your naked eye. Your body will still be feeling all the powers of the light wavelengths though!",
+      title: "Staff Management",
+      text: "Staff Management streamlines the tracking of attendance, schedules, and payroll for hostel staff. It ensures smooth operations by automating routine tasks and improving workforce efficiency.",
       icon1: <GoArrowUp />,
       icon2: <BsArrowDown />,
     },
     {
       id: 2,
-      title: "Contracts and bills",
-      text: "The near-infrared light spectrum is not visible to the human eye, and therefore any light that has a wavelength greater than around 750nm, you will not be able to see with your naked eye. Your body will still be feeling all the powers of the light wavelengths though!",
+      title: "Room Allocation",
+      text: "Room Allocation simplifies the booking and assignment of rooms to tenants, ensuring efficient space management. It tracks occupancy in real time, helping avoid overbooking and ensuring a smooth stay for guests.",
       icon1: <GoArrowUp />,
       icon2: <BsArrowDown />,
     },
     {
       id: 3,
-      title: "Utilities and rates",
-      text: "Both Red Light and NIR light are a part of the light spectrum of the sun. Red Light emits wavelengths of between 600-700 nm and near-Infrared Light (NIR) is invisible to the naked eye and has a wavelength of 700nm to 1000nm.",
+      title: "Financial Tracking",
+      text: "Financial Tracking helps manage rent collection, payments, and expenses efficiently. It provides real-time insights into hostel finances, ensuring accurate and timely financial reporting.",
       icon1: <GoArrowUp />,
       icon2: <BsArrowDown />,
     },
     {
       id: 4,
-      title: "Fixtures and inventory",
-      text: "The near-infrared light spectrum is not visible to the human eye, and therefore any light that has a wavelength greater than around 750nm, you will not be able to see with your naked eye.",
+      title: "Tenant Communication",
+      text: "Tenant Communication streamlines interactions between hostel management and tenants through instant notifications and messaging. It helps keep tenants informed about updates, maintenance, and important announcements.",
+      icon1: <GoArrowUp />,
+      icon2: <BsArrowDown />,
+    },
+    {
+      id: 5,
+      title: "Reports & Analytics",
+      text: "Reports & Analytics provide detailed insights into occupancy, revenue, and operations. It helps hostel management make data-driven decisions to optimize performance and efficiency.",
       icon1: <GoArrowUp />,
       icon2: <BsArrowDown />,
     },
@@ -44,15 +51,9 @@ function Mainfeatures() {
   };
 
   return (
+    
     <div className=" md:mx-10 mx-5 mt-10">
-      <div className="bg-[#F2FDA7]  md:p-10 p-5 rounded-2xl flex  md:flex-row flex-col items-center px-10">
-        <button className="text-white font-semibold flex justify-center text-center text-md bg-[#19467D]  py-2 lg:py-4  lg:px-16 px-10 rounded-full">
-          Start with a free plan
-        </button>
-        <h1 className=" md:text-3xl text-xl mt-5 md:mt-0 font-semibold  md:ml-10 flex text-center justify-center">
-          Experience our services with a free plan!
-        </h1>
-      </div>
+ 
 
      
 
@@ -61,63 +62,44 @@ function Mainfeatures() {
           <img
             src={mobile}
             alt="Mobile view"
-            className="rounded-2xl   h-[400px] md:h-[630px] lg:h-[600px] w-full object-cover"
+            className="rounded-2xl   h-[400px] md:h-[500px] lg:h-[500px] w-full object-cover"
+        
           />
           <div className="flex items-center gap-3   left-5 md:left-10 absolute top-10">
-            <div className="bg-[#F2FDA7] w-6 h-6 rounded-full shadow-md"></div>
-            <h1 className="text-3xl text-[#34f8f8] md:text-[40px] font-semibold">
+            <h1 className="text-3xl text-black md:text-[40px] font-semibold">
               Main features
             </h1>
           </div>
         </div>
 
         <div>
-          <div className="bg-orange-100 w-full py-5 rounded-xl p-5">
-            <h1 className="text-3xl font-semibold">Apartments</h1>
+     
 
-            <div className="grid grid-cols-2 gap-5 mt-5">
-              <div className="flex items-center gap-3 bg-white px-5 py-2 rounded-xl">
-                <FaArrowRightLong />
-                <h1 className="text-lg">Public link</h1>
-              </div>
-              <div className="flex items-center gap-3 bg-white px-5 py-2 rounded-xl">
-                <FaArrowRightLong />
-                <h1 className=" text-lg">Map location</h1>
-              </div>
-              <div className="flex items-center gap-3 bg-white px-5 py-2 rounded-xl">
-                <FaArrowRightLong />
-                <h1 className="text-lg">Tenant instructions</h1>
-              </div>
-              <div className="flex items-center gap-3 bg-white px-5 py-2 rounded-xl">
-                <FaArrowRightLong />
-                <h1 className="text-lg">Tasks</h1>
-              </div>
-            </div>
+<div className="space-y-5">
+      {data.map((service, index) => (
+        <div key={service.id} className="rounded-xl bg-[#ffb96d]">
+          <button
+            onClick={() => toggleAccordion(index)}
+            className="w-full h-full flex justify-between items-center p-5 bg-[#ffb96d] text-slate-800 rounded-xl focus:outline-none"
+          >
+            <span className="text-xl font-semibold">{service.title}</span>
+            <span className="transition-transform text-black font-bold duration-300">
+              {openIndex === index ? service.icon1 : service.icon2}
+            </span>
+          </button>
+
+          <div
+            className={`bg-[#ffb96d] text-black text-md px-5 pb-3 rounded-b-xl transition-all duration-300 overflow-hidden ${
+              openIndex === index
+                ? "max-h-screen opacity-100 visible"
+                : "max-h-0 opacity-0 invisible"
+            }`}
+          >
+            {service.text}
           </div>
-
-          <div className="space-y-5 mt-10">
-            {data.map((service, index) => (
-              <div key={service.id} className="rounded-xl bg-[#d7e4f2]">
-                <button
-                  onClick={() => toggleAccordion(index)}
-                  className="w-full h-full  flex justify-between items-center p-5 bg-[#d7e4f2] text-slate-800 rounded-xl focus:outline-none"
-                >
-                  <span className="text-xl font-semibold">{service.title}</span>
-                  <span className="transition-transform text-black font-bold duration-300">
-                    {openIndex === index ? service.icon1 : service.icon2}
-                  </span>
-                </button>
-
-                <div
-                  className={`bg-[#d7e4f2] text-black text-sm px-5 pb-3 rounded-b-xl transition-all duration-300 ${
-                    openIndex === index ? "block" : "hidden"
-                  }`}
-                >
-                  {service.text}
-                </div>
-              </div>
-            ))}
-          </div>
+        </div>
+      ))}
+    </div>
         </div>
       </div>
     </div>
--- src/Component/Home/Manage.jsx
+++ src/Component/Home/Manage.jsx
@@ -1,56 +1,60 @@
 import React from "react";
-import { MdBedroomParent } from "react-icons/md";
+import { GrUserManager } from "react-icons/gr";
+import { MdPayments } from "react-icons/md";
+import { IoMdBed } from "react-icons/io";
+import { RiCustomerServiceFill } from "react-icons/ri";
+
 
 function Manage() {
   const data = [
     {
       id: 1,
       title: "	Easy Bed & Room Booking ",
-      icon: <MdBedroomParent />,
-      text: "Hostel admins can manage bookings, room allocations, payments, and user details. They also handle feedback, services, and generate reports for efficient management.",
+      icon: <IoMdBed />,
+      text: "Easy Bed & Room Booking simplifies reservations for  PGs and hotels with an intuitive interface. It enables fast room allotments and real-time availability tracking. Owners and tenants benefit from seamless management and booking.",
     },
     {
       id: 1,
-      title: "Admin Review Management",
-      icon: <MdBedroomParent />,
-      text: "Hostel admins can manage bookings, room allocations, payments, and user details. They also handle feedback, services, and generate reports for efficient management.",
+      title: "Hostel & Staff Management",
+      icon: <GrUserManager />,
+      text: "Hostel & Staff Management simplifies staff tracking, attendance, and payroll. Automate routine tasks to improve efficiency and reduce errors. Ensure smooth operations and keep your team organized effortlessly.",
     },
     {
       id: 1,
-      title: "Admin Review Management",
-      icon: <MdBedroomParent />,
-      text: "Hostel admins can manage bookings, room allocations, payments, and user details. They also handle feedback, services, and generate reports for efficient management.",
+      title: "Payments & Security",
+      icon: <MdPayments />,
+      text: "Payments & Security simplifies rent collection with automated reminders and multiple payment options. It ensures data protection through robust security measures. Enhance trust and streamline transactions with a secure, easy-to-use system.",
     },
     {
       id: 1,
-      title: "Admin Review Management",
-      icon: <MdBedroomParent />,
-      text: "Hostel admins can manage bookings, room allocations, payments, and user details. They also handle feedback, services, and generate reports for efficient management.",
+      title: "Customer Engagement & Experience",
+      icon: <RiCustomerServiceFill />,
+      text: "Customer Engagement & Experience enhances tenant satisfaction with seamless communication, instant updates, and feedback management. Provide 24/7 booking access and location-based services to build stronger relationships and improve retention.",
     },
   ];
 
   return (
     <div>
       <div className="text-center  lg:px-60   px-10 sm:px-20  md:px-40 pt-10">
-        <h1 className=" lg:text-5xl md:text-4xl text-xl   font-semibold text-gray-400 leading-snug">
+        <h1 className=" lg:text-5xl md:text-4xl text-xl    font-semibold text-gray-400 leading-snug">
           Manage bookings, room assignments, payments, user profiles, feedback,
           services, reports, and notifications efficiently.
         </h1>
       </div>
 
       <div className=" lg:mx-10 md:mx-8  mx-5 mt-10">
-        <div className="  mt-5 rounded-3xl  bg-[#eaf5fc]   relative   p-5   lg::p-12 md:p-8   gap-3 ">
+        <div className="  mt-5 rounded-3xl  bg-[#ffb96d]   relative   p-5   lg::p-10 md:p-8   gap-3 ">
           <div className="flex items-center gap-3   left-5 md:left-10 absolute top-6">
-            <div className="bg-[#F2FDA7] w-6 h-6 rounded-full shadow-md"></div>
+   
             <h1 className="text-3xl md:text-[40px] font-semibold">Benefits</h1>
           </div>
 
-          <div className="md:grid-cols-2 grid-cols-1 grid gap-5 mt-14">
+          <div className="md:grid-cols-2 grid-cols-1  grid gap-5 mt-14">
             {data.map((value) => (
               <div key={value.id}>
-                <div className="  rounded-3xl     shadow-md  bg-white md:w-full lg:w-fit   md:p-10 lg:p-10            border-gray-500   p-5">
+                <div className="  rounded-3xl      shadow-md    bg-white md:w-full lg:w-fit   md:p-10 lg:p-10            border-gray-500   p-5">
                   <div className="flex items-center">
-                    <h1 className="   text-4xl  lg:text-5xl font-semibold text-yellow-200">
+                    <h1 className="   text-4xl  lg:text-5xl font-semibold text-[#ca3b03cc]">
                       {value.icon}
                     </h1>
                     <h1 className=" md:pl-5 pl-3   text-xl md:text-2xl   font-semibold">
